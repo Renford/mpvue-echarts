2f4e57c99d5a94faa5e4cd7dbaf0d817430bc955
断虹残雪 <chaofree3088@sina.com>
2019-04-05 18:27:42 +0800

    [Update] 不再支持使用废弃接口，如setFontSize等

--- src/wx-canvas.js
+++ src/wx-canvas.js
@@ -1,19 +1,19 @@
 export default class WxCanvas {
   constructor(ctx, canvasId) {
-    this.ctx = ctx;
-    this.canvasId = canvasId;
-    this.chart = null;
+    this.ctx = ctx
+    this.canvasId = canvasId
+    this.chart = null
 
-    WxCanvas.initStyle(ctx);
-    this.initEvent();
+    WxCanvas.initStyle(ctx)
+    this.initEvent()
   }
 
   getContext(contextType) {
-    return contextType === '2d' ? this.ctx : null;
+    return contextType === '2d' ? this.ctx : null
   }
 
   setChart(chart) {
-    this.chart = chart;
+    this.chart = chart
   }
 
   attachEvent() {
@@ -25,49 +25,51 @@ export default class WxCanvas {
   }
 
   static initStyle(ctx) {
-    const styles = ['fillStyle', 'strokeStyle', 'globalAlpha',
-      'textAlign', 'textBaseAlign', 'shadow', 'lineWidth',
-      'lineCap', 'lineJoin', 'lineDash', 'miterLimit', 'fontSize'];
+    // const styles = ['fillStyle', 'strokeStyle', 'globalAlpha', 'textAlign', 'textBaseAlign', 'shadow', 'lineWidth', 'lineCap', 'lineJoin', 'lineDash', 'miterLimit', 'fontSize'];
+    // styles.forEach((style) => {
+    //   Object.defineProperty(ctx, style, {
+    //     set: (value) => {
+    //       if ((style !== 'fillStyle' && style !== 'strokeStyle')
+    //         || (value !== 'none' && value !== null)
+    //       ) {
+    //         ctx[`set${style.charAt(0).toUpperCase()}${style.slice(1)}`](value);
+    //       }
+    //     },
+    //   });
+    // });
 
-    styles.forEach((style) => {
-      Object.defineProperty(ctx, style, {
-        set: (value) => {
-          if ((style !== 'fillStyle' && style !== 'strokeStyle')
-            || (value !== 'none' && value !== null)
-          ) {
-            ctx[`set${style.charAt(0).toUpperCase()}${style.slice(1)}`](value);
-          }
-        },
-      });
-    });
-
-    ctx.createRadialGradient = () => ctx.createCircularGradient(arguments);
+    ctx.createRadialGradient = () => ctx.createCircularGradient(arguments)
   }
 
   initEvent() {
-    this.event = {};
-    const eventNames = [{
-      wxName: 'touchStart',
-      ecName: 'mousedown',
-    }, {
-      wxName: 'touchMove',
-      ecName: 'mousemove',
-    }, {
-      wxName: 'touchEnd',
-      ecName: 'mouseup',
-    }, {
-      wxName: 'touchEnd',
-      ecName: 'click',
-    }];
+    this.event = {}
+    const eventNames = [
+      {
+        wxName: 'touchStart',
+        ecName: 'mousedown'
+      },
+      {
+        wxName: 'touchMove',
+        ecName: 'mousemove'
+      },
+      {
+        wxName: 'touchEnd',
+        ecName: 'mouseup'
+      },
+      {
+        wxName: 'touchEnd',
+        ecName: 'click'
+      }
+    ]
 
-    eventNames.forEach((name) => {
-      this.event[name.wxName] = (e) => {
-        const touch = e.mp.touches[0];
+    eventNames.forEach(name => {
+      this.event[name.wxName] = e => {
+        const touch = e.mp.touches[0]
         this.chart.getZr().handler.dispatch(name.ecName, {
           zrX: name.wxName === 'tap' ? touch.clientX : touch.x,
-          zrY: name.wxName === 'tap' ? touch.clientY : touch.y,
-        });
-      };
-    });
+          zrY: name.wxName === 'tap' ? touch.clientY : touch.y
+        })
+      }
+    })
   }
 }
